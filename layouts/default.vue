5b6db9cbbabc87c168de941ebb06a6f931825e1e
Juan Manuel Béc <Juanmabs22@hotmail.com>
2025-03-19 14:45:05 +0100

    feat: fixed ladnign page

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,11 @@ import { Analytics } from '@vercel/analytics/nuxt'
     <NuxtLoadingIndicator/>
     <AppHeader class="z-50" />
     
-    <div class="min-h-dvh pb-36">
+    <div class="min-h-dvh">
       <NuxtPage />
     </div>
     
-    <AppFooter class="absolute bottom-0 w-full z-50 lg:!border-transparent bg-neutral-50/75 dark:bg-neutral-950/75 backdrop-blur backdrop-blur w-full" />
+    <AppFooter/>
     
     <UNotifications />
     <Analytics/>
--- pages/index.vue
+++ pages/index.vue
@@ -73,45 +73,66 @@ defineOgImageScreenshot({
   delay: 2000
 })
 </script>
-
 <template>
   <div class="relative w-full h-full min-h-dvh">
-    <div class="absolute -z-10 max-h-screen max-w-screen h-full  w-full h-full">
-      <div class="min-h-80 max-h-screen max-w-screen h-full  w-full h-full">
+    <div class="absolute -z-10 max-h-screen max-w-screen h-full w-full">
+      <div class="min-h-80 max-h-screen max-w-screen h-full w-full">
         <ClientOnly>
-          <DecorationTresjs class="min-h-80 max-h-screen max-w-screen h-full w-full h-full" />
+          <DecorationTresjs class="min-h-80 max-h-screen max-w-screen h-full w-full" />
         </ClientOnly>
       </div>
     </div>
-    
-    <ULandingHero
-      :title="page.hero.title"
-      :description="page.hero.description"
-      :links="page.hero.links"
-      class="2xl:pt-[35vh] xl:pt-[25vh] pt-[15vh]"
-    >
-      <ULandingLogos
-        :title="page.logos.title"
-        align="center"
-      >
-        <UIcon
-          v-for="icon in page.logos.icons"
-          :key="icon"
-          :name="icon"
-          class="w-12 h-12 lg:w-16 lg:h-16 flex-shrink-0 text-neutral-900 dark:text-white zoom-image"
-        />
-      </ULandingLogos>
-    </ULandingHero> 
+
+    <div class="py-24 sm:py-32 md:py-40 relative 2xl:pt-[35vh] xl:pt-[25vh] pt-[15vh]">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl gap-16 sm:gap-y-24 flex flex-col">
+        <div class="text-center">
+          <h1 class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
+            {{ page.hero.title }}
+          </h1>
+          <div class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
+            {{ page.hero.description }}
+          </div>
+          <div class="mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center">
+            <a
+              v-for="link in page.hero.links"
+              :key="link.to"
+              :href="link.to"
+              :target="link.target || '_self'"
+              :class="[
+                'focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-full text-base gap-x-2.5 px-3.5 py-2.5 shadow-sm inline-flex items-center',
+                link.color === 'gray'
+                  ? 'ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700/50'
+                  : 'text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800 dark:bg-white dark:hover:bg-gray-100',
+              ]"
+            >
+              <UIcon v-if="link.icon" :name="link.icon"></UIcon>
+              <span>{{ link.label }}</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <h2 class="text-lg font-semibold leading-8 text-gray-900 dark:text-white">
+            {{ page.logos.title }}
+          </h2>
+          <div class="mx-auto mt-10 flex flex-wrap items-center justify-between gap-8">
+            <UIcon
+              v-for="icon in page.logos.icons"
+              :key="icon"
+              :name="icon"
+              class=" w-12 h-12 lg:w-16 lg:h-16 flex-shrink-0 text-gray-900 dark:text-white zoom-image"
+              aria-hidden="true"
+            ></UIcon>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="bg-primary-50/70 dark:bg-primary-40/70 backdrop-blur py-5 px-3 gap-0 lg:mx-10">
-      <div class="max-w-6xl mx-auto"> 
+      <div class="max-w-6xl mx-auto">
         <div class="flex justify-between m-2">
-          <h2 class="dark:text-slate-950"> {{ page.hero.lastBlogsPosts }} </h2>
-          <NuxtLink
-            class="focus:outline-none"
-            tabindex="-1"
-            :to="localePath('/blog')"
-          >
+          <h2 class="dark:text-slate-950 pt-2">{{ page.hero.lastBlogsPosts }}</h2>
+          <NuxtLink class="focus:outline-none" tabindex="-1" :to="localePath('/blog')">
             <UButton
               :label="page.hero.links[0].label"
               icon="i-heroicons-arrow-right-20-solid"
@@ -120,16 +141,12 @@ defineOgImageScreenshot({
             />
           </NuxtLink>
         </div>
-        
-        <div class="flex flex-wrap place-items-stretch items-stretch" v-if="blogs3">
+
+        <div class="flex flex-wrap place-items-stretch items-stretch text-gray-900 dark:text-white" v-if="blogs3">
           <template v-for="(article, index) in blogs3" :key="article.path">
             <NuxtLink :to="article.path" class="flex-1 min-w-[200px] font-semibold m-2 h-full" v-if="index < 3">
-              <UCard
-                :to="article.path"
-              >
-                <h2>
-                  {{ article.title }}
-                </h2>
+              <UCard :to="article.path">
+                <h2>{{ article.title }}</h2>
               </UCard>
             </NuxtLink>
           </template>
